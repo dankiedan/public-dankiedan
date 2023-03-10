5042d41f1499c8d0f417ad6bfe6581429e52dd9e
Daniel Kwon <d.kwon31965@gmail.com>
2023-03-11 02:52:11 +1300

    Cleaned the repo and added an Array.java file

---
+++ Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String[] args) {
+        System.out.println("This is how you print a line, and then we will print an array");
+        String stringArray[] = {"ABC", "DEF", "XYZ"};
+        int integerArray[] = {1, 2, 3, 4, 5};
+
+        System.out.println(stringArray[1]); // This will output the 1st index, DEF
+        System.out.println(integerArray[0]); // This will output the 
+    }
+}
