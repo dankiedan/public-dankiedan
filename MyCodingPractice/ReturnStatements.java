95b9beab4465922a10c4f1501eb72e1d91395805
Daniel Kwon <d.kwon31965@gmail.com>
2023-03-11 19:33:56 +1300

    Beginning a new file called ReturnStatements.java

---
+++ MyCodingPractice/ReturnStatements.java
@@ -0,0 +1,28 @@
+public class ReturnStatements {
+    public static void main(String[] args) {
+        printAMessage();
+        int x = sumOfTwoNumbers(23, 46);
+        int y = sumOfTwoNumbers(2, 223);
+        int z = sumOfTwoNumbers(12, 16);
+
+        System.out.println("The sum of the two numbers is: " + x);
+        System.out.println("The sum of the two numbers is: " + y);
+        System.out.println("The sum of the two numbers is: " + z);
+
+        // Let's learn how to shout
+        shouting("I really need to shout!");
+
+    }
+
+    public static void printAMessage() {
+        System.out.println("Hello World?");
+    }
+    
+    public static int sumOfTwoNumbers(int a, int b) {
+        return a + b; // Returning an integer
+    }
+
+    public static String shouting(String s) {
+        return s.toUpperCase(); // Return a string
+    }
+}
