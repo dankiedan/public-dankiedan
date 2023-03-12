4eb38b57ff4f1ab08d3aa0f2dae1d4da0cba43db
Daniel Kwon <d.kwon31965@gmail.com>
2023-03-12 20:18:13 +1300

    New file

---
+++ MyCodingPractice/ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+    public static void main(String[] args) {
+    
+        String[] names = {"Daniel", "Andrew", "Hanyi", "William"};
+
+        for(String index : names) {
+            System.out.println(index);
+        }
+    
+    }
+}
