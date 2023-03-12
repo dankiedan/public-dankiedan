c2612e5043af669dfe15ce2263aaf09295343a96
Daniel Kwon <d.kwon31965@gmail.com>
2023-03-12 20:11:17 +1300

    new file

--- MyCodingPractice/ArrayList.java
+++ MyCodingPractice/ArrayList.java
@@ -1,14 +1,27 @@
+import java.util.ArrayList;
+
 public class ArrayList {
-    public static void main(String[] args) {
 
-    // This is the conventional method of using arrays
-    String[] fruits = new String[3];
-    fruits[0] = "Apple";
-    fruits[1] = "Banana";
-    fruits[2] = "Cherry";
-    
-    System.out.println(fruits[1]); // This will print out fruits[1]
+	public static void main(String[] args) {
 
+		String[] fruits = new String[4];
+		fruits[0] = "Mango";
+		fruits[1] = "Apple";
+		fruits[2] = "Strawberry";
+		fruits[3] = "Watermelon";
+		System.out.println(fruits); // This breaks the array print
 
-    }
+		ArrayList fruitList = new ArrayList();
+		fruitList.add("Mango");
+		fruitList.add("Apple");
+		fruitList.add("Strawberry");
+		fruitList.add("Watermelon");
+
+		fruitList.remove("Strawberry");
+        // fruitList.clear();
+		System.out.println(fruitList.contains("Raspberry")); // Does it contain Raspberry within the fruitList?
+
+		System.out.println(fruitList); 
+	}
+
 }
