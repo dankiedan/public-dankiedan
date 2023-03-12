4fa6c3952d1f396c4040949a50bf1bce8217b692
Daniel Kwon <d.kwon31965@gmail.com>
2023-03-12 19:26:24 +1300

    Started new file for array lists

---
+++ MyCodingPractice/ArrayList.java
@@ -0,0 +1,14 @@
+public class ArrayList {
+    public static void main(String[] args) {
+
+    // This is the conventional method of using arrays
+    String[] fruits = new String[3];
+    fruits[0] = "Apple";
+    fruits[1] = "Banana";
+    fruits[2] = "Cherry";
+    
+    System.out.println(fruits[1]); // This will print out fruits[1]
+
+ 
+    }
+}
