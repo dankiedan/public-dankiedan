4cf7273288bea4853fa8c56215c8bc82099ff720
Daniel Kwon <d.kwon31965@gmail.com>
2023-03-14 17:50:24 +1300

    made some changes

---
+++ MyCodingPractice/ArrayAndListArray.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList; // This is needed for array list
+
+public class ArrayAndListArray {
+    public static void main(String[] args) {
+
+        int[] array = new int[5]; // Initialise empty 1 x 5 array
+        int[] arrayTwo = {1, 2, 3, 4, 5}; // Setting variables within array
+
+        String[] stringArray = {"Hello", "World"};
+
+        System.out.println(array[1]);
+        System.out.println(arrayTwo[2]);
+        System.out.println(stringArray[1]);
+
+
+        System.out.println("-----------------------------------"); // Print divider
+
+        ArrayList<Integer> listOfNumbers = new ArrayList<Integer>();
+        listOfNumbers.add = (256);
+        listOfNumbers.add = (512);
+        
+        System.out.println(listOfNumbers);
+
+    }
+}
--- MyCodingPractice/ArrayLists.java
+++ MyCodingPractice/ArrayLists.java
@@ -4,12 +4,12 @@ public class ArrayLists {
 
 	public static void main(String[] args) {
 
-		String[] fruits = new String[4];
+		String[] fruits = new String[4]; // This is a regular array with a fixed length
 		fruits[0] = "Mango";
 		fruits[1] = "Apple";
 		fruits[2] = "Strawberry";
 		fruits[3] = "Watermelon";
-		System.out.println(fruits); // This breaks the array print
+		System.out.println(fruits); // This breaks the array print because it requires indexing
 
 		ArrayList<String> fruitList = new ArrayList<String>();
 		fruitList.add("Mango");
